4448f0ca3203f7dd516acdc223451dbe0fb2b37a
Sadveek <s.phuyal.3082@westcliff.edu>
2026-01-29 16:38:53 +0545

    Add files via upload

---
+++ HOMEWORK_Main/week 3/js_tasks_1/evenOdd.js
@@ -0,0 +1,7 @@
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is an even number.");
+    } else {
+        console.log(number + " is an odd number.");
+    }
+}
---
+++ HOMEWORK_Main/week 3/js_tasks_1/rightTriangle.js
@@ -0,0 +1,8 @@
+function printTriangle(n) {
+    let pattern = "";
+
+    for (let i = 1; i <= n; i++) {
+        pattern += "*";
+        console.log(pattern);
+    }
+}
